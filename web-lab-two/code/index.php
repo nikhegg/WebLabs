ebc6e69c1b379dbe225f24f5e83bfa162da8dfa5
Loompard <77997488+nikhegg@users.noreply.github.com>
2024-03-08 19:15:44 +0200

    Created order variable and concatted it

--- web-lab-two/code/index.php
+++ web-lab-two/code/index.php
@@ -4,8 +4,8 @@
 
 
 // Write your code here:
-
-
+$order = &$very_bad_unclear_name;
+$order .= ", two number 9s, a number 9 large, a number 6 extra dip and a number 7";
   
- // Don't change the line below
- echo "\nYour order is: $very_bad_unclear_name.";
+// Don't change the line below
+echo "\nYour order is: $very_bad_unclear_name.";
