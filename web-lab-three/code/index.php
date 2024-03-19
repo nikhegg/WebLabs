895b4518f281768cb586db4674b69ce7c2c16000
nikhegg <77997488+nikhegg@users.noreply.github.com>
2024-03-19 18:27:16 +0200

    Update index.php to blank HTML page

--- web-lab-three/code/index.php
+++ web-lab-three/code/index.php
@@ -1,2 +1,11 @@
-<?php
-echo phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Lab Three</title>
+</head>
+<body>
+    
+</body>
+</html>
