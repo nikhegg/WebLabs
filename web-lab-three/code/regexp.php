1b44a55c6bcb548ebcb2c3a7da5e22e869ad15a5
nikhegg <77997488+nikhegg@users.noreply.github.com>
2024-03-19 18:45:00 +0200

    Add example string echo to regexp.php

--- web-lab-three/code/regexp.php
+++ web-lab-three/code/regexp.php
@@ -6,6 +6,10 @@
     <title>RegExp Task</title>
 </head>
 <body>
-    
+    <h1>RegExp</h1>
+    <?php
+    $str = 'ahb acb aeb aeeb adcb axeb';
+    ?>
+    <div>Example string: <b><?php echo $str;?></b></div>
 </body>
 </html>
