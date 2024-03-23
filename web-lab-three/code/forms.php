0ac98c452c9eea7815312bc33f7f6057176d1894
nikhegg <77997488+nikhegg@users.noreply.github.com>
2024-03-23 14:12:05 +0200

    Add text form

--- web-lab-three/code/forms.php
+++ web-lab-three/code/forms.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
     <title>Word & Symbols Count</title>
 </head>
 <body>
-    
+    <div class="shadow-container">
+        <h1>Task 1 Form</h1>
+        <form action="textcount.php">
+            <label for="text-example">Enter your text</label>
+            <br>
+            <textarea name="text-example" id="text-example" cols="50" rows="3"></textarea>
+            <br>
+            <input type="submit" value="Count words and symbols">
+        </form>
+    </div>
 </body>
 </html>
