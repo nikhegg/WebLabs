23d2b6a18b8cab6cf4e8aa04e241155267386c2d
nikhegg <77997488+nikhegg@users.noreply.github.com>
2024-03-23 22:50:31 +0200

    Create market navigation bar with CSS

--- web-lab-three/code/market/index.php
+++ web-lab-three/code/market/index.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="./market.css">
     <title>Web Market</title>
 </head>
 <body>
-    
+    <nav>
+        <div class="market-logo">Web Market</div>
+    </nav>
 </body>
 </html>
