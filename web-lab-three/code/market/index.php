0aa3b724ea6abc2de5e799f00c68ce815adcfa60
nikhegg <77997488+nikhegg@users.noreply.github.com>
2024-03-23 23:23:30 +0200

    Create layout for market

--- web-lab-three/code/market/index.php
+++ web-lab-three/code/market/index.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,5 +12,59 @@
     <nav>
         <div class="market-logo">Web Market</div>
     </nav>
+    <div id="root">
+        <div class="shadow-container">
+            <h1>Sell your product</h1>
+            <form action="./publish-product.php" method="post">
+                <label for="category">Select the category</label>
+                <select name="category" id="category" required>
+                    <!-- TODO Place foreach here -->
+                    <option value="cars">Cars</option>
+                    <option value="other">Other</option>
+                    <!-- -->
+                </select>
+                <label for="email">Your contact email</label>
+                <input type="email" name="email" required>
+                <label for="title">Product title</label>
+                <input type="text" name="title" required>
+                <label for="description">Description</label>
+                <textarea name="description" cols="60" rows="4" required></textarea>
+                <br>
+                <button type="submit">Publish</button>
+            </form>
+        </div>
+        <div id="offers-table">
+            <table>
+                <thead>
+                    <th>Category</th>
+                    <th>Title</th>
+                    <th>Email</th>
+                    <th>Description</th>
+                </thead>
+                <tbody>
+                    <!-- TODO Place foreach here -->
+                    <tr>
+                        <td>Cars</td>
+                        <td>Porsche Cayenne 2020</td>
+                        <td>test@webmarket.ru</td>
+                        <td>This was my dream car and now it's high time to sell it!</td>
+                    </tr>
+                    <tr>
+                        <td>Cars</td>
+                        <td>Porsche Cayenne 2020</td>
+                        <td>test@webmarket.ru</td>
+                        <td>This was my dream car and now it's high time to sell it!</td>
+                    </tr>
+                    <tr>
+                        <td>Cars</td>
+                        <td>Porsche Cayenne 2020</td>
+                        <td>test@webmarket.ru</td>
+                        <td>This was my dream car and now it's high time to sell it!</td>
+                    </tr>
+                    <!-- -->
+                </tbody>
+            </table>
+        </div>
+    </div>
 </body>
 </html>
