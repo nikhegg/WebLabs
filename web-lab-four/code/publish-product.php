35f00ff229690e42ad3fec2a3425107db13f2068
nikhegg <77997488+nikhegg@users.noreply.github.com>
2024-05-03 17:45:27 +0200

    Create Google client and spreadsheets service

--- web-lab-four/code/publish-product.php
+++ web-lab-four/code/publish-product.php
@@ -1,7 +1,15 @@
 <?php
 require_once 'vendor/autoload.php';
 if(true === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description'])) {
-    
+    $client = new Google_Client();
+    $client->setApplicationName('WebLab4');
+    $client->setScopes(['https://www.googleapis.com/auth/spreadsheets']);
+    $client->setAccessType('offline');
+    $client->setAuthConfig('credentials.json');
+
+    $service = new Google_Service_Sheets($client);
+    $spreadsheetId = "1bKXPauAFhzRgPlBbTjeXsAt1RMr5CbobKJfHi4Vtc-g";
+    $listName = "List1";
 }
 header('Location: /index.php');
 exit();
