52d5f6ded1ddc2fb734fa3b6bde0272278a341bd
nikhegg <77997488+nikhegg@users.noreply.github.com>
2024-05-03 17:17:06 +0200

    Composer init

---
+++ web-lab-four/code/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nick/code',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9444b64a7dcdb72fae9b54514f0240d533c01fd1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'nick/code' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9444b64a7dcdb72fae9b54514f0240d533c01fd1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
