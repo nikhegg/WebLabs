99bfea98e9864e841973a3b227a44aef799f0962
nikhegg <77997488+nikhegg@users.noreply.github.com>
2024-05-03 17:21:06 +0200

    Install google dependency

--- web-lab-four/code/vendor/composer/autoload_real.php
+++ web-lab-four/code/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit5d8c6eec01158bb5c43b574e03df13c1
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit5d8c6eec01158bb5c43b574e03df13c1', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit5d8c6eec01158bb5c43b574e03df13c1', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInit5d8c6eec01158bb5c43b574e03df13c1
         call_user_func(\Composer\Autoload\ComposerStaticInit5d8c6eec01158bb5c43b574e03df13c1::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit5d8c6eec01158bb5c43b574e03df13c1::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
