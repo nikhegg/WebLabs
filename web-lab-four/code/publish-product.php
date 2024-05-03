43a38d408e71a7fd1af4bfc7b6ba6655e5f24af7
nikhegg <77997488+nikhegg@users.noreply.github.com>
2024-05-03 18:21:22 +0200

    Add table publish algorithm

--- web-lab-four/code/publish-product.php
+++ web-lab-four/code/publish-product.php
@@ -10,6 +10,16 @@ if(true === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['
     $service = new Google_Service_Sheets($client);
     $spreadsheetId = "1bKXPauAFhzRgPlBbTjeXsAt1RMr5CbobKJfHi4Vtc-g";
     $listName = "List1";
+
+    $email = $_POST['email'];
+    $category = $_POST['category'];
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $payload = [[$email, $category, $title, $description]];
+    $body = new Google_Service_Sheets_ValueRange(['values' => $payload]);
+    $opts = array('valueInputOption' => 'USER_ENTERED');  
+
+    $service->spreadsheets_values->append($spreadsheetId, $listName, $body, $opts);
 }
 header('Location: /index.php');
 exit();
