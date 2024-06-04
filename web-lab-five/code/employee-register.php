4f6c8aa7f08058377ef9fed9e8f2fff7b658b7e9
nikhegg <77997488+nikhegg@users.noreply.github.com>
2024-06-04 21:14:39 +0200

    Remove lab5

--- web-lab-five/code/employee-register.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if(true === isset($_POST['name'], $_POST['age'], $_POST['salary'], $_POST['email'], $_POST['position'])) {
-    $_SESSION['employee'] = [$_POST['name'], $_POST['age'], $_POST['salary'], $_POST['email'], $_POST['position']];
-}
-header('Location: /forms.php');
-exit();
