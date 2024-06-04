600685f990b50312d975ef9459e4cb842721066a
nikhegg <77997488+nikhegg@users.noreply.github.com>
2024-06-04 21:37:54 +0200

    Use MySQL for data save

--- web-lab-five/code/publish-product.php
+++ web-lab-five/code/publish-product.php
@@ -1,10 +1,20 @@
 <?php
+function exitToHome(): void {
+    header('Location: /');
+    exit();
+}
 if(true === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description'])) {
     $email = $_POST['email'];
     $category = $_POST['category'];
     $title = $_POST['title'];
     $description = $_POST['description'];
-}
 
-header('Location: /');
-exit();
+    $mysqli = new mysqli('db', 'root', 'helloworld', 'web');
+    if(mysqli_connect_errno()) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exitToHome();
+    }
+
+    $mysqli->query("INSERT INTO ad (email, title, description, category) VALUES ('$email', '$title', '$description', '$category')");
+}
+exitToHome();
